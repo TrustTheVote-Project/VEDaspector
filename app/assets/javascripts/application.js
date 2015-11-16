3f830d7fe21cd80f59ded77e15e65f26494a87b9
Scott Ostler <sbostler@gmail.com>
2015-11-15 17:28:31 -0800

    Tweak inspector_title_string strings, dismiss popovers when clicking outside

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,6 +44,14 @@ function ready() {
     setupItemContainer('.string-collection');
     setupItemContainer('.internationalized-text');
 
+    $('body').on('click', function(e) {
+        $('[data-toggle=popover]').each(function () {
+            if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+                $(this).popover('hide');
+            }
+        });
+    });
+
     $('.download-button').on('click', function(e) {
         e.preventDefault();
 
